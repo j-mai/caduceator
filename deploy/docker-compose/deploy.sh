b9cbb92b63e8dcb8dd1ac63a03ee70c01368e180
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2020-10-06 13:35:54 -0700

    testing caduceus

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -9,7 +9,5 @@ ARGUS_VERSION=${ARGUS_VERSION:-0.3.6} \
 CADUCEATOR_VERSION=${CADUCEATOR_VERSION:-local} \
 docker-compose -f $ROOT_DIR/deploy/docker-compose/docker-compose.yml up -d $@
 
-sleep 10
-
 bash config_dynamodb.sh
 
